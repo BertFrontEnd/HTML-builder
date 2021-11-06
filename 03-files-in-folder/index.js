cecb97866e7c46123d760f60fc6e0b33e6fd3c5b
BertFrontEnd <Berton@yandex.ru>
2021-11-06 19:28:43 +0300

    feat: add solution of 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+const filePath = path.join(__dirname, 'secret-folder');
+
+const getFileInfo = (directory) => {
+  fs.readdir(directory, (error, files) => {
+    if (error) throw error;
+    /* console.log(files); */
+
+    for (let file of files) {
+      fs.stat(directory + '/' + file, (error, stats) => {
+        if (error) throw error;
+        if (stats.isFile()) {
+          let fullNameFile = file;
+          let fileExt =
+            path.parse(file).ext === ''
+              ? path.parse(file).name.slice(1)
+              : path.parse(file).ext.slice(1);
+          let nameFile =
+            path.parse(file).ext === '' ? 'null' : path.parse(file).name;
+          let fileSize = Number((stats.size / 1024).toFixed(3)) + 'kb';
+          console.log(
+            chalk.cyan(fullNameFile),
+            '->',
+            chalk.yellow(nameFile),
+            '-',
+            chalk.green(fileExt),
+            '-',
+            chalk.magenta(fileSize),
+          );
+        } else {
+          getFileInfo(directory + '/' + file);
+        }
+      });
+    }
+  });
+};
+
+getFileInfo(filePath);
