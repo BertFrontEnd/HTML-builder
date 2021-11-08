2dfbed9c2d31bca198903a6f388889846eceaf0a
BertFrontEnd <Berton@yandex.ru>
2021-11-08 20:18:48 +0300

    chore: edit solution of 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,56 @@
+const { readdir } = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+const srcFilePath = path.join(__dirname, 'styles');
+const distFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
+const fileExt = '.css';
+
+let arrStyles = [];
+let writeableStream = fs.createWriteStream(distFilePath);
+
+const getStyleFiles = async () => {
+  let files = await readdir(srcFilePath);
+  let cssFiles = files.filter((file) => path.extname(file) === fileExt);
+
+  return cssFiles;
+};
+
+const readStyleFiles = async (file) => {
+  let styles = '';
+  let promise = new Promise((resolve) => {
+    let readableStream = fs.createReadStream(path.resolve(srcFilePath, file));
+    readableStream.addListener('data', (data) => {
+      styles += data;
+    });
+    readableStream.addListener('end', (error) => {
+      if (error) throw error;
+      resolve(styles);
+    });
+  });
+
+  return promise;
+};
+
+const writeStylesFile = async () => {
+  arrStyles.forEach(
+    async (style) =>
+      await style.then((data) => {
+        writeableStream.write(data);
+      }),
+  );
+};
+
+const createBundle = async () => {
+  const cssFiles = await getStyleFiles();
+  cssFiles.forEach((cssFile) => {
+    arrStyles.push(readStyleFiles(cssFile));
+  });
+
+  writeStylesFile();
+};
+
+createBundle();
+
+console.log(chalk.cyan('Create bundle is done!'));
