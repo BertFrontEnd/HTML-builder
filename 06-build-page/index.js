4ecf3918f3b5ef958fcf793495697b76bd8d6839
BertFrontEnd <Berton@yandex.ru>
2021-11-09 00:22:08 +0300

    chore: edit solution of 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -8,6 +8,11 @@ const distFilePath = path.join(__dirname, 'project-dist');
 const distFile = path.join(distFilePath, 'style.css');
 const srcAssetsPath = path.join(__dirname, 'assets');
 const distAssetsPath = path.join(distFilePath, 'assets');
+
+const htmlComponentsPath = path.resolve(__dirname, 'components');
+const htmlTemplatePath = path.resolve(__dirname, 'template.html');
+const htmlMainPath = path.resolve(distFilePath, 'index.html');
+
 const fileExt = '.css';
 
 let arrStyles = [];
@@ -82,12 +87,81 @@ const getAssets = async (assetsFrom, assetsTo) => {
   });
 };
 
+const readHtmlTemplate = async () => {
+  let htmlTemplate = '';
+  const readableStream = fs.createReadStream(htmlTemplatePath);
+  const promise = new Promise((res) => {
+    readableStream.addListener('data', (data) => {
+      htmlTemplate += data;
+    });
+    readableStream.addListener('end', () => {
+      res(htmlTemplate);
+    });
+  });
+
+  return promise;
+};
+
+const getHtmlTemplates = (template) => {
+  return template.match(/\{\{.+\}\}/g);
+};
+
+const getHtmlComponentsName = async () => {
+  const htmlComponentsName = (
+    await readdir(htmlComponentsPath, {
+      withFileTypes: true,
+    })
+  )
+    .filter((file) => file.isFile() && path.extname(file.name) === '.html')
+    .map((file) => path.basename(file.name, '.html'));
+
+  return htmlComponentsName;
+};
+
+const replaceHtmlTemplate = async (template, matching) => {
+  return Promise.all(
+    matching.map(
+      (match) =>
+        new Promise((resolve) => {
+          let componentHTML = '';
+          const componentPath = path.resolve(
+            __dirname,
+            'components',
+            `${match}.html`,
+          );
+          const reg = new RegExp(`\{\{${match}\}\}`);
+          const readableTemplateStream = fs.createReadStream(componentPath);
+          readableTemplateStream.addListener('data', (data) => {
+            componentHTML += data;
+          });
+          readableTemplateStream.addListener('end', () => {
+            template = template.replace(reg, componentHTML);
+            resolve();
+          });
+        }),
+    ),
+  ).then(() => template);
+};
+
+const createHtmlLayout = async () => {
+  const writeableHtmlStream = fs.createWriteStream(htmlMainPath);
+  const htmlComponents = await getHtmlComponentsName();
+  const htmlTemplate = await readHtmlTemplate();
+  const htmlTemplates = getHtmlTemplates(htmlTemplate);
+  const htmlMatching = htmlComponents.filter((component) =>
+    htmlTemplates.includes(`{{${component}}}`),
+  );
+  const finishHtml = await replaceHtmlTemplate(htmlTemplate, htmlMatching);
+  writeableHtmlStream.write(finishHtml);
+};
+
 const init = async () => {
   await readDir();
   await createBundle();
   await getAssets(srcAssetsPath, distAssetsPath);
+  await createHtmlLayout();
 };
 
 init();
 
-console.log(chalk.cyan('Create bundle is done!'));
+console.log(chalk.cyan('Create layout is done!'));
