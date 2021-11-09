61e15eb84bfa0f35f1416827b7ee64b804d1ae9e
BertFrontEnd <Berton@yandex.ru>
2021-11-09 21:07:45 +0300

    chore: edit solution of 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-const chalk = require('chalk');
 
 const filePath = path.join(__dirname, './', 'text.txt');
 
 const readableStream = fs.createReadStream(filePath, 'utf8');
 
 readableStream.on('data', (fileContent) => {
-  const prettyText = chalk.blue(fileContent);
-  console.log(prettyText);
+  console.log(fileContent);
 });
